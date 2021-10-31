bd71fe4ac87873ad07aa0f489d55f6d8fc9c9bf5
wendy <wendy.vandenberghe@live.fr>
2021-10-31 23:52:55 +0100

    Create Article.java

---
+++ src/main/java/com/jee/model/Article.java
@@ -0,0 +1,41 @@
+package com.jee.model;
+
+public class Article {
+	private int id;
+	private String title;
+	private int quantity;
+	private double price;
+
+	public Article(String title, int quantity, double price) {
+		super();
+		this.title = title;
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+}
